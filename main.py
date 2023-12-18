b25893d893f026e5b6e6df66033356e81a832ba7
josh140520 <143544549+josh140520@users.noreply.github.com>
2023-12-18 17:22:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
-import matplotlib.pyplot as plt
+import kivy
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+import seaborn as sns
+import matplotlib.pyplot as plt
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
-class GraphApp(App):
+# Sample data
+data = sns.load_dataset("iris")
+
+# Create a Seaborn plot
+sns.set(style="whitegrid")
+fig, ax = plt.subplots()
+sns.scatterplot(x="sepal_length", y="sepal_width", hue="species", data=data, ax=ax)
+plt.title("Seaborn Scatter Plot in Kivy")
+
+# Kivy App
+class SeabornKivyApp(App):
     def build(self):
-        # Create a Kivy layout
+        # Create a box layout
         layout = BoxLayout(orientation='vertical')
 
-        # Create a Matplotlib figure and axis
-        fig, ax = plt.subplots()
-
-        # Generate some example data
-        x = [1, 2, 3, 4, 5]
-        y = [2, 4, 6, 8, 10]
-
-        # Plot the data
-        ax.plot(x, y, marker='o')
-
-        # Set labels and title
-        ax.set_xlabel('X-axis')
-        ax.set_ylabel('Y-axis')
-        ax.set_title('Matplotlib Graph in Kivy')
-
-        # Add the Matplotlib figure to the Kivy layout
-        layout.add_widget(FigureCanvasKivyAgg(fig))
+        # Embed the Seaborn plot in the Kivy app using FigureCanvasKivyAgg
+        canvas = FigureCanvasKivyAgg(plt.gcf())
+        layout.add_widget(canvas)
 
         return layout
 
 if __name__ == '__main__':
-    GraphApp().run()
+    SeabornKivyApp().run()
